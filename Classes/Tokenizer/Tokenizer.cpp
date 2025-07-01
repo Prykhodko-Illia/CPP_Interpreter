0b7645a7f4c2b56b7af3047874603b3a615dcd32
Prykhodko-Illia <iprykhodko@kse.org.ua>
2025-07-02 02:04:34 +0300

    Tokenizer class with tokenize function was written

---
+++ Classes/Tokenizer/Tokenizer.cpp
@@ -0,0 +1,50 @@
+﻿#include "Tokenizer.h"
+
+bool isNumber(const char c) {
+    for (const auto el : {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}) {
+        if (c == el) return true;
+    }
+
+    return false;
+}
+
+bool isOperand(const char c) {
+    for (const auto el : {'+', '-', '*', '/', '=', '(', ')'}) {
+        if (c == el) return true;
+    }
+
+    return false;
+}
+
+std::vector<Token> Tokenizer::tokenPartite(const std::string &text){
+    std::vector<Token> result;
+
+    std::string currentScope;
+    std::string token;
+
+    for (int i = 0; text[i] != '\0'; ++i) {
+        if (text[i] == ' ' || isOperand(text[i])) {
+            if (!token.empty()) {
+                result.emplace_back(token, currentScope);
+            }
+
+            currentScope = "";
+            token = "";
+
+            if (text[i] != ' ') result.emplace_back(text[i], "operand");
+            continue;
+        }
+
+        if (isNumber(text[i]) && currentScope != "string") {
+            if (currentScope.empty()) currentScope = "number";
+
+            token += text[i];
+        } else {
+            if (currentScope.empty()) currentScope = "string";
+
+            token += text[i];
+        }
+    }
+
+    return result;
+}
